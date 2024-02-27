6a887025264afe393880b740af24a0dff677736a
Miro Metsänheimo <miro@metsanheimo.fi>
2024-02-27 12:37:43 +0200

    fix broken url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
 	network: 'live',
 	wsUrl: 'wss://nanows.numsu.dev',
 	rpcUrl: 'https://nanoproxy.numsu.dev/proxy',
-	principalsUrl: 'https://mynano.ninja/api/accounts/principals',
+	principalsUrl: 'https://nanobrowse.com/api/reps_online',
 	explorerUrl: 'https://nanolooker.com',
 	repInfoUrl: 'https://mynano.ninja',
 	hostAccount: 'nano_3zapp5z141qpjipsb1jnjdmk49jwqy58i6u6wnyrh6x7woajeyme85shxewt',
